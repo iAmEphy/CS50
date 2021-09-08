eb4a903070642bb2eed7efabc186bd23a767a418
xyuzuki <tliu4@albany.edu>
2021-09-07 22:16:23 -0400

    Caesar/Readability
    Added caesar and finished readability

--- Ignore.c
+++ Ignore.c
@@ -16,142 +16,167 @@ int count_sentence(string word);
 
 
 
-int main(void){
-    
-    
+int main(void)
+{
+
+
 
     string input = get_string("Text: ");
-    
+
     int lettercount = count_letters(input);
-    
+
     int wordcount = count_words(input);
-    
+
     int sentencecount = count_sentence(input);
-    
+
     printf("%i letter(s)\n ", lettercount);
     printf("%i word(s)\n", wordcount);
     printf("%i sentence(s)\n", sentencecount);
-    
+
     float L = (float)lettercount / wordcount * 100;
-    
+
     float S = (float)sentencecount / wordcount * 100;
-    
+
     float index = 0.0588 * L - 0.296 * S - 15.8;
-    
+
     int final = (int)index;
-    if(index < 1){
-        
+
+    if (index < 1)
+    {
+
         printf("Before Grade 1\n");
-        
+
     }
-    
-    else if (index >= 16){ 
-        
+
+    else if (index >= 16)
+    {
         printf("Grade 16+\n");
-        
-        
+
+
     }
-    else{
+    else
+    {
         printf("Grade %.0f\n", index);
-    
+
     }
-    
-    
+
+
 }
 
-int count_letters(string word){
+int count_letters(string word)
+{
     int lettercounter = 0;
-    
+
     int wordcounter = 0;
-    
+
     for (int i = 0; i < strlen(word); i++)
     {
-        
+
         if (isupper(word[i]) || islower(word[i]))
         {
-            
+
             word[i] = tolower(word[i]);
-            
+
             for (int j = 0; j < sizeof(AtoZ); j++)
             {
-                
+
                 if (word[i] == AtoZ[j])
                 {
-                    
                     lettercounter += 1;
-                    
+
+
                 }
+
             }
-           
+
         }
         else
         {
-            
+
         }
-        
+
     }
-    
+
     return lettercounter;
 }
 
 
-int count_words(string word){
+//Word counter
+int count_words(string word)
+{
 
     int wordcounter = 0;
-    
+
     for (int i = 0; i < strlen(word); i++)
     {
-        
-        if(isalpha(word[i]) && isalpha(word[i + 1]) == '\0'){
-            
-                  wordcounter += 1;
-                  
-              }
-              if(word[i] == ';'){
-            
-                  wordcounter -= 1;
-                  
-              }
-       if(word[i] == '-'){
-            
-                  wordcounter -= 1;
-           
-       }
-       
-       else if(word[i] == '\''){
+
+        //check for empty space after word
+        if (isalpha(word[i]) && isalpha(word[i + 1]) == '\0')
+        {
+            wordcounter += 1;
+
+
+        }
+
+
+        if (word[i] == ';')
+        {
             wordcounter -= 1;
-       }
-       
+
+
+        }
+
+        if (word[i] == '-')
+        {
+            wordcounter -= 1;
+
+
+        }
+
+        if (word[i] == '\'')
+        {
+            wordcounter -= 1;
+
+
+        }
+
         else
         {
-           
+
         }
-        
-        
+
+
     }
-    
+
     return wordcounter;
 }
 
 
-int count_sentence(string word){
+//Sentence count
+int count_sentence(string word)
+{
     int sentencecounter = 0;
-    
+
+
+    //loop through sentences
     for (int i = 0; i < strlen(word); i++)
     {
-        
-        if(word[i + 1] == '.' ||  word[i + 1] == '!' || word[i + 1] == '?'){
-            
-                  sentencecounter += 1;
-                  
-              }
-       
+
+        //Checking for periods, exclamation and question marks
+        if (word[i + 1] == '.' ||  word[i + 1] == '!' || word[i + 1] == '?')
+        {
+
+            sentencecounter += 1;
+
+
+        }
+
         else
         {
-           
+
         }
-        
+
     }
-    
+
     return sentencecounter;
 }
---
+++ caesar.c
@@ -0,0 +1,10 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+
+int main(void){
+    
+}
