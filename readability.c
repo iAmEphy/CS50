41483c5edaa5c858884c895989a0782643e4e1cb
xyuzuki <tliu4@albany.edu>
2021-09-07 21:13:06 -0400

    Create readability.c
    readability. not done

---
+++ readability.c
@@ -0,0 +1,120 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int count_letters(string word);
+
+int AtoZ[] = {97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122};
+
+int points [] = {};
+
+int count_words(string word);
+
+int count_sentence(string word);
+
+int main(void){
+    
+    string input = get_string("Text: ");
+    
+    int lettercount = count_letters(input);
+    
+    int wordcount = count_words(input);
+    
+    int sentencecount = count_sentence(input);
+    
+    printf("%i letter(s)\n ", lettercount);
+    printf("%i word(s)\n", wordcount);
+    printf("%i sentence(s)\n", sentencecount);
+}
+
+int count_letters(string word){
+    int lettercounter = 0;
+    
+    int wordcounter = 0;
+    
+    for (int i = 0; i < strlen(word); i++)
+    {
+        
+        if (isupper(word[i]) || islower(word[i]))
+        {
+            
+            word[i] = tolower(word[i]);
+            
+            for (int j = 0; j < sizeof(AtoZ); j++)
+            {
+                
+                if (word[i] == AtoZ[j])
+                {
+                    
+                    lettercounter += 1;
+                    
+                }
+            }
+           
+        }
+        else
+        {
+            
+        }
+        
+    }
+    
+    return lettercounter;
+}
+
+
+int count_words(string word){
+
+    int wordcounter = 0;
+    
+    for (int i = 0; i < strlen(word); i++)
+    {
+        
+        if(isalpha(word[i]) && isalpha(word[i + 1]) == '\0'){
+            
+                  wordcounter += 1;
+                  
+              }
+              if(word[i] == ';'){
+            
+                  wordcounter -= 1;
+                  
+              }
+       if(word[i] == '-'){
+            
+                  wordcounter -= 1;
+                  
+              }
+        else
+        {
+           
+        }
+        
+    }
+    
+    return wordcounter;
+}
+
+
+int count_sentence(string word){
+    int sentencecounter = 0;
+    
+    for (int i = 0; i < strlen(word); i++)
+    {
+        
+        if(word[i + 1] == '.' ||  word[i + 1] == '!' || word[i + 1] == '?'){
+            
+                  sentencecounter += 1;
+                  
+              }
+       
+        else
+        {
+           
+        }
+        
+    }
+    
+    return sentencecounter;
+}
