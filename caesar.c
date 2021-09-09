2b94f07f3e798a17f0d7c2d50ba1aa5e66b73c24
xyuzuki <tliu4@albany.edu>
2021-09-09 19:31:38 -0400

    Update caesar.c
    Caesar's scrambler

--- caesar.c
+++ caesar.c
@@ -3,8 +3,56 @@
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
+#include <stdlib.h>
 
-
-int main(void){
+int main(int argc, string argv[]){
+    
+    
+    //Only need 1 arguement
+    if (argc != 2)
+    {
+        printf("Usage: ./caesar k\n");
+        return 1;
+    }
+    
+    //No letters, only numbers
+    for (int i = 0, n = strlen(argv[1]); i < n; i++)
+    {
+        if (!isdigit(argv[1][i]))
+        {
+            printf("Usage: ./caesar key\n");
+            return 1;
+        }
+    }
+    
+    int key = atoi(argv[1]); 
+    
+    // input
+    string input = get_string("plaintext: ");
+    
+    string result = input;
+    //Loop, change every character
+    for (int x = 0, j = strlen(input); x < j; x++){
+        
+        //letters
+        if (isalpha(input[x]))
+        {
+            //uppercase?
+            
+            if (isupper(input[x])){
+                result[x] = ((input[x] - 'A' + key) % 26) + 'A';
+            }
+            //lowercase?
+            
+            else if (islower(input[x])){
+                result[x] = ((input[x] - 'a' + key) % 26) + 'a';
+            }
+        }
+    }
+    
+    //result
+    printf("ciphertext: %s\n", result);
+    
+    return 0;
     
 }
